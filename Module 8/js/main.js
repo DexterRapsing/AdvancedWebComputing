b5905e5d11d1e8d1996acbc4239f50fa1a5a1982
Ylevol04 <lovelyanncastillo@gmail.com>
2013-09-14 03:56:30 +0800

    Update main.js

--- Module 8/js/main.js
+++ Module 8/js/main.js
@@ -1,42 +1,9 @@
 var key = "txkk8d53p6vt7twxqvb8p7ub";
 
 
-
-/*** $("#box_office").click(function(){
-	showBox();
-});
-
-	function showBox(){
-		var app = {};
-	    var url = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json';
-	    $.ajax({
-	        url: url,
-	        data: {
-	            apiKey: 'hcrurhsttexasrgfm2y6yahm'
-	        },
-	        dataType: 'jsonp',
-	        success: showMovie
-	}
-	function showMovie(response) {
-        console.log('response', response);
-        var movies = response.movies;
-        $('#movie43').replaceWith('<div id="movie43"></div>');
-        for (var i = 0; i < movies.length; i++) {
-            var movie = movies[i];
-            /*$('#page_three').append('<h3>' + movie.title + '</h3>');*
-
-            $('#movie43').append(
-             '<div id = "post1"><div class = "innerdiv"><img class = "thumb" src="' + posters.profile + '" /></div><div class = "innerdiv"><h6> '+ movie.title +'</h6></div></div>');
-           
-        }
-    } 
-***/
-
-
-
 $(function(){
 
-  $('#searchtxt').focus(function(){
+  $('input').focus(function(){
       var full = $(".divmovie43").has("img").length ? true : false;
       if(full == 'false'){
          $('.divmovie43').empty();
@@ -44,39 +11,67 @@ $(function(){
   });
 
 
-$('#searchtxt').empty();
-$('#searchbtn').click(function(){ 
-	if ($('#searchtxt').val() == '') {
-			$('h4').replaceWith('<h4> Undefined search. </h4>')
-			$('.divmovie43').replaceWith($('<div class = ".divmovie43"</div>'));
-		} 
-	else{
-		$('h4').replaceWith('<h4> Movie search reuslts. </h4>')
-		search();
-	}
-
-   
+$("input").empty();
+$("button").click(function(){ 
+    search();
  });
-$('#searchtxt').keydown(function(event){
-		if (event.which == 13) {
-			$('#searchbtn').click();
-		};
-	});
 
   function search() {
         $.ajax({
             url: 'http://api.rottentomatoes.com/api/public/v1.0/movies.json',
             dataType: 'jsonp',
             data: {
-                q: $("#searchtxt").val(),
-                apiKey: key
+                q: $("input").val(),
+                apiKey: key,
             },
-
             success: showList
-
         });
 
      }
+
+function boxMovie(){
+		var server = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json';
+		$.ajax({
+			url: server,
+			dataType: 'jsonp',
+			data: {
+				apikey: key,
+				limit: 10,
+				country: 'ph'
+			},
+			success: showMovies
+		});
+	}
+	
+	//In Theaters Movies
+	function intMovie(){
+		var server = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+		$.ajax({
+			url: server,
+			dataType: 'jsonp',
+			data: {
+				apikey: key,
+				page_limit: 10,
+				page: pageNum,
+				country: 'ph'
+			},
+			success: showMovies
+		});
+	}
+
+	function upMovie(){
+		var server = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json';
+		$.ajax({
+			url: server,
+			dataType: 'jsonp',
+			data: {
+				apikey: key,
+				page_limit: 10,
+				country: 'ph'
+			},
+			success: showMovies
+		});
+	}
 
 
     function showList(response) {
@@ -88,10 +83,10 @@ $('#searchtxt').keydown(function(event){
             /**$('#page_three').append('<h3>' + movie.title + '</h3>');**/
 
             $('#movie43').append(
-             '<div id = "post1"><div class = "innerdiv"><img class = "thumb" src="' + movie.posters.original + '" /></div><div class = "innerdiv"><h6> '+ movie.title +'</h6></div></div>');
+              '<div id = "post1"><div class = "innerdiv"><img id = "thumb" src="' + movie.posters.original + '" /></div><div class = "innerdiv"><h5> '+ movie.title +'</h5></div></div>');
+            
            
         }
     }
-
          
 });
